1318e76ff85491d1e2fb7b51cad2494bbcee605d
CryoFlar3 <ndukes@computermentors.org>
2016-02-14 23:26:14 -0500

    Added Calculate class
    I added a class that is supposed to calculate the age of the user.

--- app/src/main/java/org/computermentors/whatsmyage/UI/AgeActivity.java
+++ app/src/main/java/org/computermentors/whatsmyage/UI/AgeActivity.java
@@ -1,7 +1,9 @@
-package org.computermentors.whatsmyage;
+package org.computermentors.whatsmyage.UI;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import org.computermentors.whatsmyage.R;
 
 public class AgeActivity extends AppCompatActivity {
 
--- app/src/main/java/org/computermentors/whatsmyage/UI/MainActivity.java
+++ app/src/main/java/org/computermentors/whatsmyage/UI/MainActivity.java
@@ -1,9 +1,14 @@
-package org.computermentors.whatsmyage;
+package org.computermentors.whatsmyage.UI;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.computermentors.whatsmyage.BackEnd.Calculate;
+import org.computermentors.whatsmyage.R;
+
 public class MainActivity extends AppCompatActivity {
+
+    private Calculate mCalculate = new Calculate();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
