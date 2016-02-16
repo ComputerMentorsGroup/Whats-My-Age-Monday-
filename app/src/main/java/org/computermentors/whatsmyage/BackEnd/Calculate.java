74412b590dd2469c14a16f3e50c5e83fa3573952
Mentor <mobileapps@computermentors.org>
2016-02-15 19:06:51 -0500

    added calculations for date and age
    updated the getAge() and getDate() methods.

--- app/src/main/java/org/computermentors/whatsmyage/BackEnd/Calculate.java
+++ app/src/main/java/org/computermentors/whatsmyage/BackEnd/Calculate.java
@@ -2,6 +2,7 @@ package org.computermentors.whatsmyage.BackEnd;
 
 import android.widget.DatePicker;
 
+import java.util.Calendar;
 import java.util.Date;
 
 /**
@@ -11,14 +12,24 @@ public class Calculate {
 
     // Returns the date entered in to the datepicker
     public Date getDate (DatePicker datePicker){
-        Date date = new Date();
+        Date date = new Date(datePicker.getYear() - 1900, datePicker.getMonth(), datePicker.getDayOfMonth());
 
         return date;
     }
 
     // Returns the age of the user in years
     public int getAge (Date date){
-        int age = 0;
+
+        Calendar dob = Calendar.getInstance();
+        dob.setTime(date);
+        Calendar today = Calendar.getInstance();
+        int age = today.get(Calendar.YEAR) - dob.get(Calendar.YEAR);
+        if (today.get(Calendar.MONTH) < dob.get(Calendar.MONTH)) {
+            age--;
+        } else if (today.get(Calendar.MONTH) == dob.get(Calendar.MONTH)
+                && today.get(Calendar.DAY_OF_MONTH) < dob.get(Calendar.DAY_OF_MONTH)) {
+            age--;
+        }
 
         return age;
     }
